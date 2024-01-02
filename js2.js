0c9f54fa7b29eebb4f2c135f32c1bad5971dc975
Manthan Adhau <123251485+ManthanAdhau@users.noreply.github.com>
2024-01-02 21:44:44 +0530

    js2.js

--- js2.js
+++ js2.js
@@ -1,7 +1,21 @@
 const bar= document.getElementById('bar');
 const close= document.getElementById('close');
 const nav= document.getElementById('navbar');
+let btn = document.getElementById("btn");
 
+let btnIcon = document.getElementById("btnIcon");
+
+btn.onclick = function(){
+   document.body.classList.toggle("dark-theme");
+   if(document.body.classList.contains("dark-theme")){
+      btnIcon.src ="sun1.png";
+      
+   }else{
+      btnIcon.src ="moon1.png";
+     
+   }
+
+}
 
 if(bar){
    bar.addEventListener('click', ()=>{
@@ -13,32 +27,4 @@ if(close){
      nav.classList.remove('active');
     })
  }
-// function showSidebar(){
-//    const sidebar = document.querySelector('.sidebar')
-// }
-const cart = {
-   items: [],
-   addItem: function (id, name, price) {
-     this.items.push({ id, name, price });
-     this.updateCart();
-   },
-   updateCart: function () {
-     const cartItemsElement = document.getElementById('pro');
-     cartItemsElement.innerHTML = '';
 
-     this.items.forEach(item => {
-       const li = document.createElement('li');
-       li.textContent = `${item.name} - $${item.price}`;
-       cartItemsElement.appendChild(li);
-     });
-   }
-   
- };
- function addToCart(button) {
-   const productElement = button.closest('.pro');
-   const id = productElement.getAttribute('data-id');
-   const name = productElement.getAttribute('data-name');
-   const price = productElement.getAttribute('data-price');
-
-   cart.addItem(id, name, price);
- }
